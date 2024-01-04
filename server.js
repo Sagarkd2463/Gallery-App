57f0c39b23198be27e0b782473cbc2414e4e6491
Sagar Deshpande <101045627+Sagarkd2463@users.noreply.github.com>
2024-01-04 19:15:01 +0530

    Added Files

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const { json } = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(cors());
+app.use(json());
+
+const { parsed: config } = dotenv.config();
+
+const BASE_URL = `https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}/resources/image`;
+
+const BASE_URL_SEARCH = `https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}/resources/search`;
+
+const auth = {
+    username: config.API_KEY,
+    password: config.API_SECRET,
+}
+ 
+app.get('/photos', async (req, res) => {
+    const response = await axios.get(BASE_URL, {
+        auth,
+        params: {
+            next_cursor: req.query.next_cursor
+        }
+    });
+
+    return res.send(response.data);
+});
+
+app.get('/search', async (req, res) => {
+    const response = await axios.get(BASE_URL_SEARCH, {
+        auth,
+        params: {
+            expression: req.query.expression
+        }
+    });
+    return res.send(response.data);
+});
+
+const PORT = 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
